6c173ba6e5072f07d08d045536716c07a9973fec
ErfanMasoudiBA <erfanmasoudi@gmail.com>
2024-11-08 18:59:48 +0330

    add routing for forgetpassword page

--- src/features/Auth/ForgetPassword/forgotPassword.jsx
+++ src/features/Auth/ForgetPassword/forgotPassword.jsx
@@ -1,5 +1,5 @@
-import '../../../src/App.css'
-import React, { useState } from 'react';
+// import '../../../src/App.css'
+import React, { useState, useEffect } from "react";
 import {
   Box,
   Button,
@@ -15,33 +15,38 @@ import {
   Link,
 } from "@mui/material";
 
-
 const ForgotPassword = () => {
-    const [email, setEmail] = useState('');
-    const [errorMessage, setErrorMessage] = useState("");
-    const [success, setSuccess] = useState('');
-    const [loading, setLoading] = useState(false);
-    
-    const handleEmailChange = (e) => {
-      setEmail(e.target.value);
-      
-    }
-      
-    const validateEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [success, setSuccess] = useState("");
+  const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    // Add the background class when the component is mounted
+    document.body.classList.add("signup-background");
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-
-      if (!validateEmail(email)) {
-        setErrorMessage("Invalid email format");
-        return;
-
+    // Clean up by removing the class when the component is unmounted
+    return () => {
+      document.body.classList.remove("signup-background");
     };
+  }, []);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
   };
-    return (
-      
-      <Container
+
+  const validateEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setErrorMessage("Invalid email format");
+      return;
+    }
+  };
+  return (
+    <Container
       maxWidth="xs"
       sx={{
         display: "flex",
@@ -49,8 +54,8 @@ const ForgotPassword = () => {
         justifyContent: "center",
         minHeight: "100vh",
       }}
-      > 
-        <Box
+    >
+      <Box
         sx={{
           display: "flex",
           flexDirection: "column",
@@ -68,45 +73,44 @@ const ForgotPassword = () => {
 
         <form onSubmit={handleSubmit}>
           <div className="form-group">
-            
-        <Typography variant="p" gutterBottom>
-            enter your Email Address to recieve a password reset link
-        </Typography>
+            <Typography variant="p" gutterBottom>
+              enter your Email Address to recieve a password reset link
+            </Typography>
 
             <TextField
-          fullWidth
-          label="Email"
-          variant="outlined"
-          margin="normal"
-          value={email}
-          onChange={handleEmailChange}
-          error={Boolean(errorMessage && !validateEmail(email))}
-          helperText={
-            !validateEmail(email)
-              ? "Enter a valid email address (e.g., name@example.com)."
-              : ""
-          }
-        />
+              fullWidth
+              label="Email"
+              variant="outlined"
+              margin="normal"
+              value={email}
+              onChange={handleEmailChange}
+              error={Boolean(errorMessage && !validateEmail(email))}
+              helperText={
+                !validateEmail(email)
+                  ? "Enter a valid email address (e.g., name@example.com)."
+                  : ""
+              }
+            />
           </div>
-      
-          
+
           <Button
-          variant="contained"
-          color="primary"
-          fullWidth
-          sx={{ mt: 2, mb: 2 }}
-          onClick={handleSubmit}
-          disabled={loading}
-        >
-          {loading ? <CircularProgress size={24} color="inherit" /> : "Reset Password"}
-        </Button>
+            variant="contained"
+            color="primary"
+            fullWidth
+            sx={{ mt: 2, mb: 2 }}
+            onClick={handleSubmit}
+            disabled={loading}
+          >
+            {loading ? (
+              <CircularProgress size={24} color="inherit" />
+            ) : (
+              "Reset Password"
+            )}
+          </Button>
         </form>
-        </Box>
-      </Container>
-      
-      
-    );
-  
-};  
+      </Box>
+    </Container>
+  );
+};
 
 export default ForgotPassword;
