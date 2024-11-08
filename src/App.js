50b34d27b4c1494db5c772f6c2b980b59118a515
ILLIDAN <116190146+Crauly53@users.noreply.github.com>
2024-11-08 11:17:03 +0330

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'
+import ResetPassword from './components/forgotPassword/resetPassword';
+import ForgotPassword from './components/forgotPassword/forgotPassword';
+
+function App() {
+  return(
+  <div>
+    <ForgotPassword />
+    <ResetPassword />
+    
+  </div>
+  )
+}
+
+export default App;
