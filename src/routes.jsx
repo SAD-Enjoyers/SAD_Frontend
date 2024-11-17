f141b8b391f7ddec01e87a780cda7e74af10e39f
Mohammadreza-Edrisabadi <75852598+Mohammadreza-Edrisabadi@users.noreply.github.com>
2024-11-17 19:05:51 +0330

    Add route for QuestionPage component functionality

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,7 @@ import Home from "./features/Home/Home";
 import Signup from "./features/Auth/Signup/Signup.jsx";
 import Login from "./features/Auth/Login/Login.jsx";
 import Fpass from "./features/Auth/ForgetPassword/forgotPassword.jsx";
+import QuestionPage from "./features/Exams/QuestionPage/QuestionPage";
 
 function AppRoutes() {
   return (
@@ -13,6 +14,7 @@ function AppRoutes() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/fp" element={<Fpass />} />
+        <Route path="/QuestionPage" element={<QuestionPage/>} />
       </Routes>
     </Router>
   );
