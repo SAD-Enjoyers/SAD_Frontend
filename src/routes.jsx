0ff3fabdcef062482d050c78befe2df6a3f6738d
Mohammadreza-Edrisabadi <75852598+Mohammadreza-Edrisabadi@users.noreply.github.com>
2024-11-22 03:45:36 +0330

    Add ExamsTab.jsx component, mock examData.json, setup routes, and clean up code

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,7 @@ import Home from "./features/Home/Home";
 import Signup from "./features/Auth/Signup/Signup.jsx";
 import Login from "./features/Auth/Login/Login.jsx";
 import Fpass from "./features/Auth/ForgetPassword/forgotPassword.jsx";
+import ExamsTab from "./features/Exams/QuestionSearch/ExamsTab.jsx";
 
 function AppRoutes() {
   return (
@@ -13,6 +14,7 @@ function AppRoutes() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/fp" element={<Fpass />} />
+        <Route path="/examsearch" element={<ExamsTab />} />
       </Routes>
     </Router>
   );
