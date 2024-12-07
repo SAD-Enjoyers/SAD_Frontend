b02d3e527b947d8ca818da267f92b77553bcc417
Mohammadreza-Edrisabadi <75852598+Mohammadreza-Edrisabadi@users.noreply.github.com>
2024-12-07 04:06:18 +0330

    feat: add AddArticle component with text editor and toast notifications

---
+++ src/features/Articles/AddArticle/AddArticle.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import {
+  Box,
+  TextField,
+  Button,
+  Typography,
+  Container,
+  Paper,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@mui/material";
+import "react-quill/dist/quill.snow.css";
+import ReactQuill from "react-quill";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { keyframes } from '@mui/system';
