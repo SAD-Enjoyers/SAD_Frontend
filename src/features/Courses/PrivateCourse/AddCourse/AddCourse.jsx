abd7c382a4abd48c07e6ec1d8fdd26c904e26146
Mohammadreza-Edrisabadi <75852598+Mohammadreza-Edrisabadi@users.noreply.github.com>
2024-12-20 18:48:46 +0330

    Setup: Import required libraries and dependencies

---
+++ src/features/Courses/PrivateCourse/AddCourse/AddCourse.jsx
@@ -0,0 +1,24 @@
+import {
+    Box,
+    Button,
+    Container,
+    Grid,
+    TextField,
+    Typography,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+    Checkbox,
+    ListItemText,
+    FormHelperText,
+    CircularProgress,
+  } from "@mui/material";
+  import {
+    MonetizationOn,
+    AccessTime,
+    School,
+    UploadFile,
+  } from "@mui/icons-material";
+  import React, { useState } from "react";
+  
