b9659d613613a5fcd628e72d1e55ceca35ce5a02
Crauly53 <a.karami.534320@gmail.com>
2025-01-25 14:50:59 +0330

    fix: solve confilct

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import AppRoutes from "./routes.jsx"; // Use .jsx extension
+import ChartPage from "./features/Admin/adminPanel.jsx";
 // import PrivateProfile from "./features/Home/PrivateProfile.jsx";
 function App() {
   return (
     <div className="App" margin={0}>
-      <AppRoutes />
+      <ChartPage />
     </div>
   );
 }
