4a70543d299dfb8677d31a9f43502b967f3f43a6
amir-ghadrdan <amirhsynqdrdan81@gmail.com>
2025-01-25 18:25:58 +0330

    add: admin route

--- src/routes.jsx
+++ src/routes.jsx
@@ -44,6 +44,7 @@ import PublicUsers from "./features/Users/publicUsers/PublicUsers.jsx";
 
 import TicketAdmin from "./features/Admin/ticketReview/ticketReview.jsx";
 import RegisterTicket from "./common/registerTicket/registerTicket.jsx";
+import Admin from "./features/Admin/main.jsx";
 
 function AppRoutes() {
   return (
@@ -51,61 +52,70 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<Layout />}>
           {/* Admin */}
+          =
+          <Route path="admin" element={<Admin />} />
           <Route path="TicketAdmin" element={<TicketAdmin />} />
           <Route path="RegisterTicket" element={<RegisterTicket />} />
-
           {/* General Routes */}
-          <Route index element={<Home />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="login" element={<Login />} />
-          <Route path="fp" element={<Fpass />} />
-          <Route path="changepassword" element={<ChangePassword />} />
-          <Route path="profile" element={<PrivateProfile />} />
-          <Route path="PublicUsers/:userId" element={<PublicUsers />} />
-          <Route path="WalletPage" element={<WalletPage />} />
-          <Route path="TermsAndConditions" element={<TermsAndConditions />} />
-          <Route path="QA" element={<QA />} />
-          <Route path="AboutUs" element={<AboutUs />} />
+          <>
+            <Route index element={<Home />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="login" element={<Login />} />
+            <Route path="fp" element={<Fpass />} />
+            <Route path="changepassword" element={<ChangePassword />} />
+            <Route path="profile" element={<PrivateProfile />} />
+            <Route path="PublicUsers/:userId" element={<PublicUsers />} />
+            <Route path="WalletPage" element={<WalletPage />} />
+            <Route path="TermsAndConditions" element={<TermsAndConditions />} />
+            <Route path="QA" element={<QA />} />
+            <Route path="AboutUs" element={<AboutUs />} />
 
-          {/* Exams */}
-          <Route path="PublicExam" element={<PublicExam />} />
-          <Route path="ExamResult" element={<ExamResult />} />
-          <Route path="PrivateExam/:serviceId" element={<PrivateExam />} />
-          <Route path="make_exam" element={<MakeExam />} />
-          <Route path="QuestionBank" element={<QuestionBank />} />
-          <Route path="ExamPreview/:serviceId" element={<ExamPreview />} />
-          <Route path="OngoingExamPage" element={<OngoingExamPage />} />
-          <Route path="image" element={<ImageUpload />} />
-          <Route path="ExamSearch" element={<ExamSearch />} />
+            {/* Exams */}
+            <Route path="PublicExam" element={<PublicExam />} />
+            <Route path="ExamResult" element={<ExamResult />} />
+            <Route path="PrivateExam/:serviceId" element={<PrivateExam />} />
+            <Route path="make_exam" element={<MakeExam />} />
+            <Route path="QuestionBank" element={<QuestionBank />} />
+            <Route path="ExamPreview/:serviceId" element={<ExamPreview />} />
+            <Route path="OngoingExamPage" element={<OngoingExamPage />} />
+            <Route path="image" element={<ImageUpload />} />
+            <Route path="ExamSearch" element={<ExamSearch />} />
 
-          {/* Questions */}
-          <Route path="QuestionSearch" element={<QuestionSearch />} />
-          <Route path="AddQuestion" element={<AddQuestion />} />
-          <Route path="QuestionPage/:questionId" element={<QuestionPage />} />
+            {/* Questions */}
+            <Route path="QuestionSearch" element={<QuestionSearch />} />
+            <Route path="AddQuestion" element={<AddQuestion />} />
+            <Route path="QuestionPage/:questionId" element={<QuestionPage />} />
 
-          {/* Courses */}
-          <Route path="PublicCourse" element={<PublicCourse />} />
-          <Route path="PrivateCourse/:serviceId" element={<PrivateCourse />} />
-          <Route path="AddCourse" element={<AddCourse />} />
-          <Route path="CoursePreview/:serviceId" element={<CoursePreview />} />
-          <Route path="SearchCourse" element={<SearchCourse />} />
+            {/* Courses */}
+            <Route path="PublicCourse" element={<PublicCourse />} />
+            <Route
+              path="PrivateCourse/:serviceId"
+              element={<PrivateCourse />}
+            />
+            <Route path="AddCourse" element={<AddCourse />} />
+            <Route
+              path="CoursePreview/:serviceId"
+              element={<CoursePreview />}
+            />
+            <Route path="SearchCourse" element={<SearchCourse />} />
 
-          {/* Articles */}
-          <Route path="AddArticle" element={<AddArticle />} />
-          <Route path="SearchArticle" element={<ArticleSearch />} />
-          <Route path="PublicArticle" element={<PublicArticle />} />
-          <Route
-            path="PrivateArticle/:serviceId"
-            element={<PrivateArticle />}
-          />
-          <Route
-            path="ArticlePreview/:serviceId"
-            element={<ArticlePreview />}
-          />
+            {/* Articles */}
+            <Route path="AddArticle" element={<AddArticle />} />
+            <Route path="SearchArticle" element={<ArticleSearch />} />
+            <Route path="PublicArticle" element={<PublicArticle />} />
+            <Route
+              path="PrivateArticle/:serviceId"
+              element={<PrivateArticle />}
+            />
+            <Route
+              path="ArticlePreview/:serviceId"
+              element={<ArticlePreview />}
+            />
 
-          {/* Utilities */}
-          <Route path="Comments" element={<Comments />} />
-          <Route path="SearchUsers" element={<SearchUsers />} />
+            {/* Utilities */}
+            <Route path="Comments" element={<Comments />} />
+            <Route path="SearchUsers" element={<SearchUsers />} />
+          </>
         </Route>
       </Routes>
     </Router>
