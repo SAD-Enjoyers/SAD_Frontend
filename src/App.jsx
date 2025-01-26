077947e92fb9bf3f6ca9328f0a413fcf42a6fdf8
Crauly53 <a.karami.534320@gmail.com>
2025-01-26 12:45:16 +0330

    solve conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import AppRoutes from "./routes.jsx"; // Use .jsx extension
-import ChartPage from "./features/Admin/adminPanel.jsx";
 // import PrivateProfile from "./features/Home/PrivateProfile.jsx";
 function App() {
   return (
     <div className="App" margin={0}>
-      <ChartPage />
+      <AppRoutes />
     </div>
   );
 }
