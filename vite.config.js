b0ea8dc99b7d2e456c5b550950124fa074fece56
ErfanMasoudiBA <erfanmasoudi@gmail.com>
2025-01-27 17:32:29 +0330

    fix: signup connect to back deploy

--- vite.config.js
+++ vite.config.js
@@ -6,20 +6,20 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "http://thetechverse.ir:3000/api/", // General backend URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
       "/signuppage": {
-        target: "http://91.132.50.155:3000", // Localhost backend
+        target: "http://localhost:3000", // Localhost backend
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/signuppage/, "/api"), // Rewrite to match the backend route
       },
       "/forgetPassword": {
-        target: "http://91.132.50.155:3000", // Localhost backend
+        target: "http://localhost:3000", // Localhost backend
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/forgetPassword/, "/api"), // Rewrite to match the backend route
+      },
+      "/api": {
+        target: "http://thetechverse.ir:3000/api/", // General backend URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
