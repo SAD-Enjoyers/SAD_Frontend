2ef58603fdbf652a1a7004cb3a86cb4c8ae0e5b9
ErfanMasoudiBA <erfanmasoudi@gmail.com>
2025-01-27 17:39:23 +0330

    fix: signup connect to back deploy

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,30 @@
-// vite.config.js
+// // vite.config.js
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/signuppage": {
+//         target: "http://localhost:3000", // Localhost backend
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/signuppage/, "/api"), // Rewrite to match the backend route
+//       },
+//       "/forgetPassword": {
+//         target: "http://localhost:3000", // Localhost backend
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/forgetPassword/, "/api"), // Rewrite to match the backend route
+//       },
+//       "/api": {
+//         target: "http://thetechverse.ir:3000/api/", // General backend URL
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ""),
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -6,15 +32,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/signuppage": {
-        target: "http://localhost:3000", // Localhost backend
+      "/api/v1/auth/signup": {
+        target: "http://localhost:3000", // Localhost for the signup route
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/signuppage/, "/api"), // Rewrite to match the backend route
-      },
-      "/forgetPassword": {
-        target: "http://localhost:3000", // Localhost backend
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/forgetPassword/, "/api"), // Rewrite to match the backend route
+        rewrite: (path) =>
+          path.replace(/^\/api\/v1\/auth\/signup/, "/v1/auth/signup"),
       },
       "/api": {
         target: "http://thetechverse.ir:3000/api/", // General backend URL
